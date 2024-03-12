86bcc671110637fd5cd49b915b1fbabe046691fd
dehwyy <dehwyy@yandex.ru>
2024-03-12 22:10:49 +0300

    [frontend] - pages templates

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -11,6 +11,24 @@ class Routes:
   @app.route("/")
   def index():
     return render_template("index.html")
+
+  @app.route('/users')
+  def users():
+    # perform api request
+    users = [
+      {"username": "dehwyy", "picture": "https://sun9-77.userapi.com/impg/HJquvixJDUoQ1Z78ZHfU_kr-w9Lf0yamaP-DCQ/VDPE0j1FlhU.jpg?size=900x1600&quality=95&sign=f5ed202bcf80d5002f0c89bcfd79d523&type=album"},
+      {"username": "Waypo1nt", "picture": "https://avatars.githubusercontent.com/u/103949460?s=400&u=a0c696f5c8881967fcab5516e90bf04d4fd423a4&v=4"}
+    ]
+    return render_template("users.html", users=users)
+
+  @app.route("/user/<username>")
+  def user(username: str):
+    # perfrom api request
+    return render_template("user.html", username=username)
+
+  @app.route("/api/oauth2/github")
+  def github():
+    return redirect("/")
 
 
 if __name__ == '__main__':
